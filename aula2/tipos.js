da4b61757a9f4907093224095ee8c634dca01c21
grbalmeida <g.r.almeida@live.com>
2023-09-26 10:19:50 -0300

    Tipos de dados e valores

---
+++ aula2/tipos.js
@@ -0,0 +1,19 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@firma.com",
+    telefones: ["11555555550", "11444444440"],
+};
+
+console.log(cliente);
+/*
+{
+  nome: 'João',
+  idade: 24,
+  email: 'joao@firma.com',
+  telefones: [ '11555555550', '11444444440' ]
+}
+*/
+
+console.log(cliente.telefones);
+// [ '11555555550', '11444444440' ]
