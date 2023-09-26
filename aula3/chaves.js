13d56c489906a8da660ce558f3e5f3575f9f1ed3
grbalmeida <g.r.almeida@live.com>
2023-09-26 14:02:34 -0300

    Métodos de objeto

---
+++ aula3/chaves.js
@@ -0,0 +1,24 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@firma.com",
+    telefones: ["11555555550", "11444444440"],
+};
+
+// cliente.enderecos = [
+//     {
+//         rua: "R. Joseph Climber",
+//         numero: 1337,
+//         apartamento: true,
+//         complemento: "ap 934",
+//     }
+// ];
+
+const chavesDoObjetos = Object.keys(cliente);
+
+console.log(chavesDoObjetos);
+// [ 'nome', 'idade', 'email', 'telefones', 'enderecos' ]
+
+if (!chavesDoObjetos.includes("enderecos")) {
+    console.error("Erro. É necessário ter um endereço cadastrado.")
+}
