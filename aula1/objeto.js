08205b1e57c6e9ce278a03dcc137f95ec1565a44
grbalmeida <g.r.almeida@live.com>
2023-09-26 09:15:06 -0300

    O que são Objetos

---
+++ aula1/objeto.js
@@ -0,0 +1,13 @@
+const listaCPFs = [14420145803, 25337152807, 40030817811];
+
+const informacoesPessoa = ["nome", "José", "idade", 32, "CPF", "14420145803"];
+
+console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+    nome: "José",
+    idade: 32,
+    CPF: "14420145803"
+};
+
+console.log(objetoPessoa);
